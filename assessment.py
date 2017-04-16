9ecf440ca7385e667444cb3010a5534446129623
dozetal <diane.osborne@gmail.com>
2017-04-16 16:26:37 -0700

    Dictionaries - Discussion

---
+++ assessment.py
@@ -0,0 +1,43 @@
+"""
+Part 1: Discussion 
+
+
+1. What are the three main design advantages that object orientation
+   can provide? Explain each concept.
+
+
+2. What is a class?
+
+
+3. What is an instance attribute?  
+	An instance attribute is an attribute specific to an instantiation of a class. 
+	Example:  ANIMAL is the super class. CAT, DOG and WHALE are sub classes.
+	An instance attribute of CAT is specific to CAT, WHALE to WHALE.
+	The instance attribute may or may not be relevant to other animals.   
+	Example: 
+	    All CATs are species: cat, habitat: land, breathes underwater: False. 
+	    All WHALES are species: whale, habitat: ocean, breathes underwater: True. 
+
+4. What is a method?
+	
+
+5. What is an instance in object orientation?
+	
+
+6. How is a class attribute different than an instance attribute?
+   Give an example of when you might use each.
+
+	Example: 
+	    Super Class = ANIMALS
+	    	species: None
+	    	habitat: None
+	    	fur: None
+	    	habitat: None
+	    	mammal: False
+	    	food: None
+
+
+"""
+
+# Parts 2 through 5:
+# Create your classes and class methods
